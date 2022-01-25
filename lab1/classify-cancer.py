9eaffefdb6b6eefcefc10dcd787c0da103cdb62c
Chuchum <910253@gmail.com>
2022-01-25 00:11:18 +0000

    t push

--- lab1/classify-cancer.py
+++ lab1/classify-cancer.py
@@ -5,6 +5,19 @@ from sklearn.datasets import load_breast_cancer
 bc = load_breast_cancer()
 
 plot_step = 0.02
-X = bc[:, [1, 3]]
-Y = bc.target
-print(bc)
+X = bc.data[:, [1, 3]]
+y = bc.target
+
+bc_tree = tree.DecisionTreeClassifier(criterion="entropy").fit(X, y)
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step), np.arange(y_min, y_max, plot_step))
+
+Z = bc_tree.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+cs = plt.contourf(xx, yy, Z, cmap=plt.cm.Paired)
+plt.scatter(X[:, 0], X[:, 1], c=y.astype(np.float64))
+
+plt.xlabel( bc.feature_names[1], fontsize=10 )
+plt.ylabel( bc.feature_names[3], fontsize=10 )
+plt.show()
