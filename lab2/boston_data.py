f277b5d19486dbdc2ea7b8f5021b8abeb68b07c7
Chuchum <910253@gmail.com>
2022-02-04 19:15:33 +0000

    fix input parameters

--- lab2/boston_data.py
+++ lab2/boston_data.py
@@ -6,15 +6,15 @@ from sklearn import linear_model
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_boston
-from gradient_descent import gradient_descent, multi_gradient_descent, compute_loss
+from my_linear_regression import my_regression, mean_square_error
 
 if __name__ == "__main__":
     boston = load_boston()
     print('running 1 feature gradient descent...')
     X_train, X_test, y_train, y_test = train_test_split(boston.data[:, np.newaxis, 5], boston.target, test_size=0.2, random_state=0)
-    w_0, w_1 = gradient_descent(X_train, y_train, lr=0.008)
-    loss = compute_loss(X_test, y_test, w_0, w_1)
-    print("Handcraft Mean squared error: %.2f" % loss)
+    model = my_regression(X_train, y_train, lr=0.008)
+    mse = mean_square_error(X_test, y_test, model)
+    print("Handcraft Mean squared error: %.2f" % mse)
 
     regr = linear_model.LinearRegression()
     regr.fit(X_train, y_train)
@@ -24,9 +24,9 @@ if __name__ == "__main__":
     ##
     print('\nrunning 3 feature gradient descent...')
     X_train, X_test, y_train, y_test = train_test_split(boston.data[:, 3:6], boston.target, test_size=0.2, random_state=0)
-    w_0, w_1, w_2, w_3 = multi_gradient_descent(X_train, y_train, lr=0.008)
-    loss = compute_loss(X_test, y_test, w_0, w_1, w_2, w_3)
-    print("Handcraft Mean squared error: %.2f" % loss)
+    w = my_regression(X_train, y_train, lr=0.008)
+    mse = mean_square_error(X_test, y_test, w)
+    print("Handcraft Mean squared error: %.2f" % mse)
 
     regr = linear_model.LinearRegression()
     regr.fit(X_train, y_train)
